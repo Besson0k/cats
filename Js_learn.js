7713360af0905eecbb4462550ea716d60cefa942
Evgeniya <unis.tayn@gmail.com>
2017-10-17 20:10:05 +0300

    sort users

--- Js_learn.js
+++ Js_learn.js
@@ -1,24 +1,27 @@
-function makeBuffer() {
-    var value = '';
-    function buffer (piece) {
-        if (arguments.length === 0) {
-            return value;
-        }
-        value += piece;
+var users = [{
+    name: "Вася",
+    surname: 'Иванов',
+    age: 20
+}, {
+    name: "Петя",
+    surname: 'Чапаев',
+    age: 25
+}, {
+    name: "Маша",
+    surname: 'Медведева',
+    age: 18
+}];
+
+function byField(field) {
+    return function (a, b) {
+        return a[field] > b[field] ? 1 : -1;
     }
-    buffer.clear = function() {
-      value = '';
-    };
-    return buffer;
 }
-
-var buffer = makeBuffer();
-
-
-buffer("Тест");
-buffer(" тебя не съест ");
-alert( buffer() ); // Тест тебя не съест
-
-buffer.clear();
-
-alert( buffer() );
+users.sort(byField('name'));
+users.forEach(function(user) {
+alert( user.name );
+});
+users.sort(byField('age'));
+users.forEach(function(user) {
+alert( user.name );
+});
