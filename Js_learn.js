22633eb363f3bc083c7f67f52528a9ed27140cf4
Evgeniya <unis.tayn@gmail.com>
2017-10-18 16:06:02 +0300

    this user and admin

--- Js_learn.js
+++ Js_learn.js
@@ -1,27 +1,12 @@
-var users = [{
-    name: "Вася",
-    surname: 'Иванов',
-    age: 20
-}, {
-    name: "Петя",
-    surname: 'Чапаев',
-    age: 25
-}, {
-    name: "Маша",
-    surname: 'Медведева',
-    age: 18
-}];
+var user = { firstName: 'Vasya'};
+var admin = { firstName: 'Admin'};
 
-function byField(field) {
-    return function (a, b) {
-        return a[field] > b[field] ? 1 : -1;
-    }
+function func() {
+    alert ( this.firstName );
 }
-users.sort(byField('name'));
-users.forEach(function(user) {
-alert( user.name );
-});
-users.sort(byField('age'));
-users.forEach(function(user) {
-alert( user.name );
-});
+
+user.f = func;
+admin.g = func;
+
+user.f();
+admin.g();
