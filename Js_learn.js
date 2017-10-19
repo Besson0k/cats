8c9fb8f0bae773069bbfb0fb89ae078e9e37ff10
Evgeniya <unis.tayn@gmail.com>
2017-10-20 01:28:45 +0300

    get_set

--- Js_learn.js
+++ Js_learn.js
@@ -1,45 +1,18 @@
-function Calculator() {
-    var methods = {
-        "-": function(a, b) {
-            return a - b;
-        },
-        "+": function(a, b) {
-            return a + b;
-        }
-    };
-    this.calculate = function(str) {
-        var split = str.split(' '),
-            a = +split[0],
-            op = split[1],
-            b = +split[2];
-
-        if (!methods[op] || isNaN(a) || isNaN(b)) {
-            return NaN;
-        }
-        return methods[op](a, b);
-    };
-    this.addMethod = function(name, func) {
-        methods[name] = func;
-    };
+var user = {
+    firstName: "Filya",
+    surname: "Ivanov"
 }
 
-var calc = new Calculator;
-
-calc.addMethod("*", function(a, b) {
-    return a * b;
+Object.defineProperty(user, "fullName", {
+    get: function() {
+        return this.firstName + ' ' + this.surname;
+    },
+    set: function(value) {
+        var split = value.split(' ');
+        this.firstName = split[0];
+        this.surname = split[1];
+    }
 });
-calc.addMethod("/", function(a, b) {
-    return a / b;
-});
-calc.addMethod("**", function(a, b) {
-    return Math.pow(a, b);
-});
-
-var result = calc.calculate("2 ** 3");
-alert( result );
-
-var result2 = calc.calculate("2 + 3");
-alert( result2 );
-
-var result3 = calc.calculate("5 - 3");
-alert( result3 );
+user.fullName = "Vasya Pypkin";
+alert( user.firstName );
+alert( user.surname );
