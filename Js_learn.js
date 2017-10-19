343184b411259e49f48037f0c5253aa444a272be
Evgeniya <unis.tayn@gmail.com>
2017-10-20 01:36:50 +0300

    get_set_new_correct

--- Js_learn.js
+++ Js_learn.js
@@ -1,18 +1,17 @@
 var user = {
     firstName: "Filya",
-    surname: "Ivanov"
-}
-
-Object.defineProperty(user, "fullName", {
-    get: function() {
+    surname: "Ivanov",
+    get fullName() {
         return this.firstName + ' ' + this.surname;
     },
-    set: function(value) {
+    set fullName(value) {
         var split = value.split(' ');
         this.firstName = split[0];
         this.surname = split[1];
     }
-});
+};
+
+alert (user.fullName);
 user.fullName = "Vasya Pypkin";
 alert( user.firstName );
 alert( user.surname );
