ba950f27f3cdfd2b5c8021634c717b41d96883af
Evgeniya <unis.tayn@gmail.com>
2017-10-19 17:17:10 +0300

    Calculator_2 with add func

--- Js_learn.js
+++ Js_learn.js
@@ -1,11 +1,45 @@
-function Accumulator(startingValue) {
-    this.value = startingValue;
-    this.read = function () {
-        this.value = +prompt("???", 0);
-    }
+function Calculator() {
+    var methods = {
+        "-": function(a, b) {
+            return a - b;
+        },
+        "+": function(a, b) {
+            return a + b;
+        }
+    };
+    this.calculate = function(str) {
+        var split = str.split(' '),
+            a = +split[0],
+            op = split[1],
+            b = +split[2];
+
+        if (!methods[op] || isNaN(a) || isNaN(b)) {
+            return NaN;
+        }
+        return methods[op](a, b);
+    };
+    this.addMethod = function(name, func) {
+        methods[name] = func;
+    };
 }
 
-var accumulator = new Accumulator(1);
-accumulator.read();
-accumulator.read();
-alert( accumulator.value );
+var calc = new Calculator;
+
+calc.addMethod("*", function(a, b) {
+    return a * b;
+});
+calc.addMethod("/", function(a, b) {
+    return a / b;
+});
+calc.addMethod("**", function(a, b) {
+    return Math.pow(a, b);
+});
+
+var result = calc.calculate("2 ** 3");
+alert( result );
+
+var result2 = calc.calculate("2 + 3");
+alert( result2 );
+
+var result3 = calc.calculate("5 - 3");
+alert( result3 );
