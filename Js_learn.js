421e7fc37972f0b07075a446976e4339196a2d12
Evgeniya <unis.tayn@gmail.com>
2017-10-21 02:36:02 +0300

    call FIO

--- Js_learn.js
+++ Js_learn.js
@@ -1,30 +1,12 @@
-function User(fullName) {
-    this.fullName = fullName;
-    Object.defineProperties(this, {
-        
-        firstName: {
-            get: function () {
-                return this.fullName.split(" ")[0];
-            },
-            set: function (newFirstName) {
-                this.fullName = newFirstName + " " + this.lastName;
-            }
-        },
-        lastName: {
-            get: function () {
-                return this.fullName.split(" ")[1];
-            },
-            set: function (newLastName) {
-                this.fullName = this.firstName + " " + newLastName;
-            }
-        }
-    });
+function showFullName(firstName, lastPart) {
+    alert( this[firstName] + " " + this[lastPart] );
 }
 
-var vasya = new User("Vasya Pypkin");
-alert( vasya.firstName );
-alert( vasya.lastName );
+var user = {
+    firstName: "Василий",
+    surname: "Петров",
+    patronym: "Иванович"
+};
 
-vasya.lastName = "Ivanov";
-
-alert ( vasya.fullName);
+showFullName.call(user, 'firstName', 'surname');
+showFullName.call(user, 'firstName', 'patronym');
