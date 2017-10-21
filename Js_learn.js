bf00ca2d18bc16777236e884401688bf58e910fa
Evgeniya <unis.tayn@gmail.com>
2017-10-21 03:59:50 +0300

    call for arguments

--- Js_learn.js
+++ Js_learn.js
@@ -1,12 +1,8 @@
-function showFullName(firstName, lastPart) {
-    alert( this[firstName] + " " + this[lastPart] );
+function printArgs() {
+    var join = [].join;
+
+    var argStr = join.call(arguments, ':');
+    alert( argStr );
 }
 
-var user = {
-    firstName: "Василий",
-    surname: "Петров",
-    patronym: "Иванович"
-};
-
-showFullName.call(user, 'firstName', 'surname');
-showFullName.call(user, 'firstName', 'patronym');
+printArgs(1, 2, 3);
