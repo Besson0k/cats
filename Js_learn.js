8a5573b31546ab4aa523d7db763a479412dc74b3
Evgeniya <unis.tayn@gmail.com>
2017-10-21 05:41:39 +0300

    func_call_arguments

--- Js_learn.js
+++ Js_learn.js
@@ -1,7 +1,20 @@
-function sumArgs() {
+function sum() {
     return [].reduce.call(arguments, function (a, b) {
         return a + b;
     });
 }
 
-alert( sumArgs(1, 2, 3) );
+function mul() {
+    return [].reduce.call(arguments, function (a, b) {
+        return a * b;
+    })
+}
+
+function applyAll(func) {
+    return func.apply(this, [].slice.call(arguments, 1));
+}
+
+alert( applyAll(sum, 1, 2, 3) );
+alert( applyAll(mul, 2, 3, 4) );
+alert( applyAll(Math.max, 2, -2, 3) );
+alert( applyAll(Math.min, 2, -2, 3) );
