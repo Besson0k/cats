ac492e2063ef3d11ee8b69cb3d77dc55b0f366ca
Evgeniya <unis.tayn@gmail.com>
2017-10-22 16:14:01 +0300

    bird fly :)

--- Js_learn.js
+++ Js_learn.js
@@ -1,6 +1,15 @@
-var user = {
-    firstName: "Vasya",
-    sayHi: function () {
-        alert( this.firstName);
-    }
-};setTimeout(user.sayHi.bind(user), 1000);
+function ask(question, answer, ok, fail) {
+    var result = prompt(question, '');
+    if (result.toLowerCase() == answer.toLowerCase()) ok();
+    else fail();
+}
+
+ask("Выпустить птичку?", "да", fly, die);
+
+function fly() {
+    alert( 'улетела :)' );
+}
+
+function die() {
+    alert( 'птичку жалко :(' );
+}
