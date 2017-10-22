473629ea565a4bba49ec138cb5692ae9aff9d081
Evgeniya <unis.tayn@gmail.com>
2017-10-22 21:58:28 +0300

    duck typing

--- Js_learn.js
+++ Js_learn.js
@@ -1,24 +1,11 @@
-function f(x) {
-    return Math.random() * x;
+alert( {}.toString.call(123) );
+alert( {}.toString.call("строка") );
+alert( Array.isArray(new Date) );
+alert( Array.isArray([1, 2, 3]) );
+
+var x = new Date();
+
+if (x.getTime) {
+    alert( 'Дата!' );
+    alert( x.getTime() );
 }
-
-function makeCaching(f) { 
-    var cach = {};
-    return function(x) {
-        if (!(x in cach)) {
-            cach[x] = f.call(this, x);
-        }
-        return cach[x];
-    };
-}
-
-f = makeCaching(f);
-
-var a, b;
-
-a = f(1);
-b = f(1);
-alert( a == b );
-
-b = f(2);
-alert( a == b );
