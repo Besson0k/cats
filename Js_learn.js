e1e044f8b5a378c9d6fcc10ede9e95c1be54a17e
Evgeniya <unis.tayn@gmail.com>
2017-10-22 15:35:10 +0300

    bind ???

--- Js_learn.js
+++ Js_learn.js
@@ -1,13 +1,14 @@
-function blind(func, context) {
-    return function () {
+'use strict';
+function bind(func, context) {
+    return function() { // (*)
         return func.apply(context, arguments);
     };
 }
 
-function f() {
-    alert (this);
+function f(a, b) {
+    alert( this );
+    alert( a + b );
 }
 
-var g = blind(f, "Context");
-
-g();
+var g = bind(f, "Context");
+g(1, 2);
